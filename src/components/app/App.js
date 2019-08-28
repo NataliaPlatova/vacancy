61fbd81f2ca5bf0af641ed0b2a1046c5f41bb905
Natalia <n.platova@hotmail.com>
2019-08-28 17:06:29 +0700

    Hello world

---
+++ src/components/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import VacancyList from "../vacanylist/vacancyList";
+
+function App() {
+  return (
+    <VacancyList/>
+  );
+}
+
+export default App;
