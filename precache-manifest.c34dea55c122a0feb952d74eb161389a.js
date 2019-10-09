b550c2ce1f76e0089a085fcedccad028529d42fd
Natalia <n.platova@hotmail.com>
2019-10-09 22:22:21 +0700

    Updates

---
+++ precache-manifest.c34dea55c122a0feb952d74eb161389a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c13b43d305dda81f9117de46e3698d2",
+    "url": "/vacancy/index.html"
+  },
+  {
+    "revision": "d44a3f8184cf5fd56fd4",
+    "url": "/vacancy/static/css/main.81c5ab0b.chunk.css"
+  },
+  {
+    "revision": "f7f8cf332c15afe0a355",
+    "url": "/vacancy/static/js/2.df676e35.chunk.js"
+  },
+  {
+    "revision": "d44a3f8184cf5fd56fd4",
+    "url": "/vacancy/static/js/main.1f5c936f.chunk.js"
+  },
+  {
+    "revision": "9fa474b258cfddce7019",
+    "url": "/vacancy/static/js/runtime~main.ee76d30e.js"
+  }
+]);
